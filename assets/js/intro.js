b005464e65059b0debb8c02d11d586291f655260
RafaelVas-dev <rafaelbassovas@gmail.com>
2025-05-09 23:11:49 -0300

    add transição das sections

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,14 +1,15 @@
 // Seções Principais
 const secaoIntro = document.querySelector('#intro');
 const secaoLogin = document.querySelector('#login');
+const inicioTxt = document.querySelector('.intro-text');
 // Videos
 const introVideo = document.querySelector('#intro-video');
 const loginVideo = document.querySelector('#login-video');
-// Elementos
-const tituloIntro = document.querySelector('#titulo-intro');
-const paragrafoIntro = document.querySelector('#paragrafo-intro');
+// Audíos
+const somIntro = document.getElementById('som-intro');
+const somClique = document.getElementById('som-clique');
 
-// Var Cont
+// Var Controle
 let introFinalizada = false; // controla se a animação da intro já foi feita
 let loginVisivel = false;    // controla se o card de login está na tela
 
@@ -16,13 +17,23 @@ let loginVisivel = false;    // controla se o card de login está na tela
 
 
 // 1. Inicialização
-window.onload = () => {
-    mostrarIntro();
-};
 
-function mostrarIntro(){
-    introVideo.addEventListener('loadeddata', () => {
-        tituloIntro.classList.add('animar-titulo');
-        paragrafoIntro.classList.add('animar-paragrafo');
-    });
-}
+// Reproduzir som de clique + iniciar som da intro ao clicar
+document.addEventListener('click', () => {
+    somClique.currentTime = 0;
+    somClique.play();
+
+    if(!introFinalizada){
+        inicioTxt.classList.add('zoom-out');
+        setTimeout(() => {
+            secaoIntro.style.display = "none";
+            secaoLogin.classList.add('mostrar-login');
+            loginVisivel = true;
+          }, 1000);
+    }
+  
+    // Iniciar música de fundo se ainda não estiver tocando
+    if (somIntro.paused) {
+      somIntro.play();
+    }
+});
